a8b4e43098538a124f59895bc34b3baaf0c56ece
vlad <saladp9@gmail.com>
2021-06-26 16:15:50 -0600

    Restructuring and sliding friction

--- car.py
+++ car.py
@@ -1,38 +1,41 @@
 import time
 import pygame
-
+from math import *
+from config_var import *
+import numpy as np
 
 class Car:
 
-    def __init__(self, x, y, image, speed):
-        self.speed = speed
-        self.max_speed = 500 #pixels
-        self.acceleration = 85 #pixels
+    def __init__(self, x, y, image, start_speed):
+        self.position = [x, y]
+        self.velocity = 0
+        self.acceleration = 0
+        self.pointing_vector = [0, 0]
+
         self.angle = 0
         self.rotate_factor = 0
-        self.x = x
-        self.y = y
-        self.dx = 0
-        self.dy = 0
         self.source_image = pygame.image.load(image).convert()
         self.source_image = pygame.transform.scale(self.source_image, (50,25))
         self.image = self.source_image
-        self.rect = self.image.get_rect().move(self.x,self.y)
-
+        self.rect = self.image.get_rect().move(self.position)
 
     def rotate_sequence(self):
-        self.angle = self.rotate_factor % 360
+        self.angle = self.rotate_factor % 360 #Prevent rotation overflow
         self.image = pygame.transform.rotate(self.source_image, self.angle)
+
+        #Keeps the car in place when rotating
         x,y = self.rect.center
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
 
     def update_pos(self):
-        self.rect = self.image.get_rect(topleft=(self.x, self.y)) #Set rectangle to match x and y
+        self.pointing_vector = [(cos(radians(self.angle))), (sin(radians(self.angle)))]
+        self.acceleration = 0
+        self.rect = self.image.get_rect(topleft=self.position)
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_w]:
-            self.x = self.x + self.speed
+
+        #Rotation
         if pressed[pygame.K_a]:
             self.rotate_factor += .1
 
@@ -41,9 +44,27 @@ class Car:
 
         if self.rotate_factor != 0:
             self.rotate_sequence()
-            print(self.image.get_size())
 
-        # self.speed = self.speed + self.acceleration
+        #Movement
+        if pressed[pygame.K_w]:
+            self.acceleration = ACCELERATION_CONSTANT
 
+        if abs(self.velocity) < MAX_SPEED:
+            self.velocity += self.acceleration
+        else:
+            self.velocity = MAX_SPEED
 
+        #Friction
+        if self.acceleration == 0 and self.velocity > 0:
+            self.velocity -= FRICTION
+            self.velocity = max(0, self.velocity)
 
+        if self.acceleration == 0 and self.velocity < 0:
+            self.velocity += FRICTION
+            self.velocity = max(0, self.velocity)
+
+        dx = self.velocity * self.pointing_vector[0]
+        dy = self.velocity * self.pointing_vector[1]
+        self.position = [self.position[0] + dx, self.position[1] - dy] #Subtract from y since top corner is Y=0
+
+        print("Pos: " + str(self.position) + " Velocity: " + str(self.velocity) + " Acc: " + str(self.acceleration))
--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
 from car import Car
 
-#Intializes pygame
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1200, 700))
+        self.player_car = Car(280, 280, 'Assets/car.png', .05)
+
+    def game_loop(self):
+        running = True
+        while running:
+            self.screen.fill((255, 255, 255))  # Background drawn first
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.blit(self.player_car.image, self.player_car.rect)
+            pygame.draw.rect(self.screen, (10, 10, 10), self.player_car.rect, 1)
+            self.player_car.update_pos()
+            pygame.display.update()
+
+
+
 pygame.init()
-
-#Screen intialization, WidthxHeight
-screen = pygame.display.set_mode((800,600))
-
-#Car intialization
-player_car = Car(280, 280, 'Assets/car.png', .05)
-
-
-#Running Game Loop: Anything persistent must be in here
-running = True
-while running:
-    screen.fill((255, 255, 255)) #Background drawn first
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.blit(player_car.image, player_car.rect)
-    pygame.draw.rect(player_car.image,(10,10,10), player_car.rect, 1)
-    player_car.update_pos()
-    pygame.display.update()
+Game().game_loop()
